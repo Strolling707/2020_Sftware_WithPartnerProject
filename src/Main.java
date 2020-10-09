9c419e4603d85bd47ea9271236f22f46f9e8d036
Strolling707 <408090032@qq.com>
2020-10-09 21:35:30 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import userInterface.SignUI;
+
+//主函数
+public class Main {
+    public static void main(String[] args) {
+        SignUI frame = new SignUI();
+        frame.setVisible(true);
+    }
+}
